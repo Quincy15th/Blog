74cf9a4effc1437299bee95066479448105ebf83
Quincy15th <anh751715@gmail.com>
2026-03-15 22:12:48 +0700

    go

---
+++ client/src/components/admin/Login.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Login = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="w-full max-w-sm p-6 max-md:m-6 border border-primary/30 shadow-xl shadow-primary/15 rounded-lg">
+        <div className="flex flex-col items-center justify-center">
+          <div className="w-full py-6 text-center">
+            <h1 className="text-3xl font-bold">
+              <span className="text-primary">Admin</span>
+              Login
+            </h1>
+            <p className="font-light">
+              Enter your credentials to access the admin panel
+            </p>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <div className="flex flex-col">
+              <label htmlFor="">Email</label>
+              <input type="email" placeholder="your email id" required />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="">Password</label>
+              <input
+                type="password"
+                placeholder="your password"
+                required
+                className="border-b-2 border-gray-300 p-2 outline-none mb-6"
+              />
+            </div>
+            <div></div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
