601501cc9ed694442e389a27c332b6bf2acdac17
Quincy15th <anh751715@gmail.com>
2026-03-16 17:23:31 +0700

    fe

--- client/src/pages/admin/Comments.jsx
+++ client/src/pages/admin/Comments.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { comments_data } from "../../assets/assets";
+import CommentTableItem from "../../components/admin/CommentTableItem";
 const Comments = () => {
   const [comments, setComments] = useState([]);
   const [filter, setFilter] = useState("Not approved");
@@ -15,16 +16,50 @@ const Comments = () => {
         <h1>Comments</h1>
         <div className="flex gap-4">
           <button
+            onClick={() => setFilter("Approved")}
             className={`shadow-custom-sm border rounded-full px-4 py-1 cursor-pointer text-xs ${filter === "Approved" ? "text-primary" : "text-gray-700"}`}
           >
             Approved
           </button>
           <button
+            onClick={() => setFilter("Rejected")}
             className={`shadow-custom-sm border rounded-full px-4 py-1 cursor-pointer text-xs ${filter === "Rejected" ? "text-primary" : "text-gray-700"}`}
           >
             Rejected
           </button>
         </div>
+      </div>
+      <div className="relative h-4/5 max-w-3xl overflow-x-auto mt-4 bg-white shadow rounded-lg scrollbar-hide">
+        <table className="w-full text-sm text-gray-500">
+          <thead className="text-xs text-gray-700 text-left uppercase">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Blog Title & Comment
+              </th>
+              <th scope="col" className="px-6 py-3 max-sm:hidden">
+                Date
+              </th>
+              <th scope="col" className="px-6 py-3 ">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {comments
+              .filter((comment) => {
+                if (filter === "Approved") return comment.isApproved === true;
+                return comment.isApproved === false;
+              })
+              .map((comment, index) => (
+                <CommentTableItem
+                  index={index + 1}
+                  key={comment._id}
+                  comment={comment}
+                  fetchComments={fetchComments}
+                />
+              ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
