86a3c69307ce288416ee41b8c3c98054fa161747
Quincy15th <anh751715@gmail.com>
2026-03-18 08:19:09 +0700

    be env

---
+++ server/controllers/blogController.js
@@ -0,0 +1,10 @@
+export const addBlog = async (req, res) => {
+  try {
+    const { title, subTitle, description, category, image, isPublished } =
+      JSON.parse(req.body.blog);
+    const imageFile = req.file;
+    if (!title || !description || !category || !imageFile) {
+      return res.json({ success: false, message: "Missing required fields" });
+    }
+  } catch (error) {}
+};
