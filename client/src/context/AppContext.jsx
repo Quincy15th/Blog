55577cac671efcc493173f9609e797997bf75772
Quincy15th <anh751715@gmail.com>
2026-03-20 10:50:01 +0700

    connect api

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,5 +1,48 @@
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import toast from "react-hot-toast";
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 
 const AppContext = createContext();
 
-export const AppProvider = ({}) => {};
+export const AppProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [token, setToken] = useState(null);
+  const [blogs, setBlogs] = useState([]);
+  const [input, setInput] = useState("");
+
+  const fetchBlogs = async () => {
+    try {
+      const { data } = await axios.get("/api/blog/all");
+      data.success ? setBlogs(data.blogs) : toast.error(data.message);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchBlogs();
+    const token = localStorage.getItem("token");
+    if (token) {
+      setToken(token);
+      axios.defaults.headers.common["Authorization"] = ` ${token}`;
+    }
+  }, []);
+  const value = {
+    axios,
+    token,
+    navigate,
+    setToken,
+    blogs,
+    setBlogs,
+    input,
+    setInput,
+  };
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export const useAppContext = () => {
+  return useContext(AppContext);
+};
